1667c87e42e774acdaad1c345e3a8829b197cc2b
Kang Hyunwoo <shawnkang0827@outlook.com>
2022-07-13 14:08:51 +0800

    A

---
+++ Mingle (Kang Hyunwoo)/Mingle (Kang Hyunwoo)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mingle (Kang Hyunwoo)
+//
+//  Created by 강현우 on 2022/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
